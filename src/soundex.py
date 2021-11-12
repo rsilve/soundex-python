00ca8acd83dba2950cb394cef7427e2c3e06c553
Robert Silve <robert@silve.net>
2021-11-12 20:32:01 +0100

    add mypy config and rename encode method

--- src/soundex.py
+++ src/soundex.py
@@ -5,48 +5,48 @@ PREVIOUS_MATCH = "\\g<1>"
 EMPTY_CHAR = "0000"
 
 
-def encode(word: str) -> str:
+def encode_soundex_word(word: str) -> str:
     """Encode the parameter with soundex algorithm."""
     if not word:
         return EMPTY_CHAR
     word = word.upper()
-    letters = ignore_non_letter_char(word)
+    letters = _ignore_non_letter_char(word)
     if not letters:
         return EMPTY_CHAR
-    first = extract_first_char(letters)
+    first = _extract_first_char(letters)
     encoded = first
     if len(letters) > 1:
-        numbered = map_consonant_to_number(letters)
-        cleaned = remove_h_w_y(numbered)
-        cleaned = remove_adjacent_number(cleaned)
-        cleaned = remove_vowels(cleaned)
+        numbered = _map_consonant_to_number(letters)
+        cleaned = _remove_h_w_y(numbered)
+        cleaned = _remove_adjacent_number(cleaned)
+        cleaned = _remove_vowels(cleaned)
         encoded = f"{first}{cleaned[1:]}"
-    padded = pads_zero(encoded)
-    truncated = truncate(padded)
+    padded = _pads_zero(encoded)
+    truncated = _truncate(padded)
     return truncated
 
 
-def truncate(word: str) -> str:
+def _truncate(word: str) -> str:
     """Keep only the 4 first char."""
     return word[0:4]
 
 
-def remove_vowels(word: str) -> str:
+def _remove_vowels(word: str) -> str:
     """Remove vowels from the word (not if in first position)."""
     return re.sub("(.)[AEIOU]", PREVIOUS_MATCH, word)
 
 
-def remove_h_w_y(word: str) -> str:
+def _remove_h_w_y(word: str) -> str:
     """Remove the letters H W Y."""
     return re.sub("(.)[HWY]", PREVIOUS_MATCH, word)
 
 
-def remove_adjacent_number(word: str) -> str:
+def _remove_adjacent_number(word: str) -> str:
     """Keep only one number if two adjacent identical number."""
     return re.sub("(\\d)\\1+", PREVIOUS_MATCH, word)
 
 
-def map_consonant_to_number(word: str) -> str:
+def _map_consonant_to_number(word: str) -> str:
     """Replace letters by number."""
     word = re.sub(r"[BFPV]", "1", word)
     word = re.sub(r"[CGJKQSXZ]", "2", word)
@@ -57,12 +57,12 @@ def map_consonant_to_number(word: str) -> str:
     return word
 
 
-def ignore_non_letter_char(word: str) -> str:
+def _ignore_non_letter_char(word: str) -> str:
     """Keep only letters."""
     return re.sub("[^A-Z]", "", word)
 
 
-def pads_zero(word: str) -> str:
+def _pads_zero(word: str) -> str:
     """Add zeros on the right to have at least 4 characters."""
     diff = 4 - len(word)
     if diff > 0:
@@ -71,7 +71,7 @@ def pads_zero(word: str) -> str:
         return word
 
 
-def extract_first_char(word: str) -> str:
+def _extract_first_char(word: str) -> str:
     """Extract the first char of the word."""
     head = word[0]
     return head
--- tests/test_soundex.py
+++ tests/test_soundex.py
@@ -1,80 +1,79 @@
 from unittest import TestCase
 
-from src import soundex
+from src.soundex import encode_soundex_word
 
 
 class Test(TestCase):
-
     def test_soundex(self):
-        res = soundex.encode("r")
+        res = encode_soundex_word("r")
         self.assertEqual(res, "R000")
-        res = soundex.encode("robert")
+        res = encode_soundex_word("robert")
         self.assertEqual(res, "R163")
-        res = soundex.encode("robbert")
+        res = encode_soundex_word("robbert")
         self.assertEqual(res, "R163")
-        res = soundex.encode("")
-        self.assertEqual(res, '0000')
-        res = soundex.encode("Honeyman")
-        self.assertEqual(res, 'H555')
-        res = soundex.encode("Rupert")
-        self.assertEqual(res, 'R163')
-        res = soundex.encode("Ashcraft")
-        self.assertEqual(res, 'A261')
-        res = soundex.encode("Tymczak")
-        self.assertEqual(res, 'T522')
+        res = encode_soundex_word("")
+        self.assertEqual(res, "0000")
+        res = encode_soundex_word("Honeyman")
+        self.assertEqual(res, "H555")
+        res = encode_soundex_word("Rupert")
+        self.assertEqual(res, "R163")
+        res = encode_soundex_word("Ashcraft")
+        self.assertEqual(res, "A261")
+        res = encode_soundex_word("Tymczak")
+        self.assertEqual(res, "T522")
 
     def test_retain_first_letter(self):
-        res = soundex.encode("r")
+        res = encode_soundex_word("r")
         self.assertEqual(res[0], "R")
-        res = soundex.encode("")
+        res = encode_soundex_word("")
         self.assertEqual(res[0], "0")
-        res = soundex.encode("robert")
+        res = encode_soundex_word("robert")
         self.assertEqual(res[0], "R")
-        res = soundex.encode("robbert")
+        res = encode_soundex_word("robbert")
         self.assertEqual(res[0], "R")
-        res = soundex.encode("Honeyman")
-        self.assertEqual(res[0], 'H')
-        res = soundex.encode("Rupert")
-        self.assertEqual(res[0], 'R')
-        res = soundex.encode("Ashcraft")
-        self.assertEqual(res[0], 'A')
-        res = soundex.encode("T")
-        self.assertEqual(res[0], 'T')
+        res = encode_soundex_word("Honeyman")
+        self.assertEqual(res[0], "H")
+        res = encode_soundex_word("Rupert")
+        self.assertEqual(res[0], "R")
+        res = encode_soundex_word("Ashcraft")
+        self.assertEqual(res[0], "A")
+        res = encode_soundex_word("T")
+        self.assertEqual(res[0], "T")
 
     def test_zero_padding(self):
-        res = soundex.encode("r")
+        res = encode_soundex_word("r")
         self.assertEqual(res, "R000")
 
     def test_ignore_non_letter_char(self):
-        res = soundex.encode("r1234")
+        res = encode_soundex_word("r1234")
         self.assertEqual(res, "R000")
-        res = soundex.encode("1234")
+        res = encode_soundex_word("1234")
         self.assertEqual(res, "0000")
 
     def test_map_consonant_to_number(self):
-        res = soundex.encode("rtvn")
+        res = encode_soundex_word("rtvn")
         self.assertEqual(res, "R315")
 
     def test_remove_adjacent_number(self):
-        res = soundex.encode("rtdn")
+        res = encode_soundex_word("rtdn")
         self.assertEqual(res, "R350")
 
     def test_clean_first_letter(self):
-        res = soundex.encode("cgtn")
+        res = encode_soundex_word("cgtn")
         self.assertEqual(res, "C350")
 
     def test_remove_separated_by_h(self):
-        res = soundex.encode("rthd")
+        res = encode_soundex_word("rthd")
         self.assertEqual(res, "R300")
-        res = soundex.encode("hcwq")
+        res = encode_soundex_word("hcwq")
         self.assertEqual(res, "H200")
 
     def test_remove_separated_by_vowels(self):
-        res = soundex.encode("rtac")
+        res = encode_soundex_word("rtac")
         self.assertEqual(res, "R320")
-        res = soundex.encode("hetc")
+        res = encode_soundex_word("hetc")
         self.assertEqual(res, "H320")
 
     def test_truncate(self):
-        res = soundex.encode("Ashcraft")
-        self.assertEqual(res, 'A261')
+        res = encode_soundex_word("Ashcraft")
+        self.assertEqual(res, "A261")
